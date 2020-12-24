c540ba8b7ffa3f7afa6c344fe78fc743dd2bb1fd
Davi Rodrigues <davi@bee.com.br>
2020-12-24 16:35:50 -0300

    add home section component

---
+++ src/components/Home/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import devImg from "../../assets/figura-dev.png";
+import { Container, Content, Left, Right } from "./styles";
+
+function Home() {
+  return (
+    <Container>
+      <Content>
+        <Left>
+          <h2>Hi, my name is Davi</h2>
+          <p>
+            I’m a full stack developer and lover of user interface design.
+            Currentily I work with the tecnologies React.js, Node.js, React
+            Native and Figma.{" "}
+          </p>
+        </Left>
+        <Right>
+          <img src={devImg} alt="Dev Figure" />
+        </Right>
+      </Content>
+    </Container>
+  );
+}
+
+export default Home;
---
+++ src/components/Home/styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  margin-top: 70px;
+  height: 698px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(180deg, #1b1b1b 0%, #313131 100%);
+`;
+
+export const Content = styled.div`
+  max-width: 1140px;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Left = styled.div`
+  max-width: 470px;
+
+  align-items: center;
+
+  h2 {
+    font-size: 24px;
+    margin-bottom: 28px;
+  }
+
+  p {
+    font-size: 18px;
+  }
+`;
+
+export const Right = styled.div`
+  align-items: center;
+
+  img {
+    width: 405px;
+  }
+`;
